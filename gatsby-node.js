10a36d3eb1b3f52df6d77075609bf43e4b306604
Robson alves <robson.alves@olxbr.com>
2020-03-15 20:55:17 -0300

    add script to create page for each post

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,3 +19,50 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+// create page for each post
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+          next {
+            frontmatter {
+              title
+            }
+            fields {
+              slug
+            }
+          }
+          previous {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.map(({ node, next, previous }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve('./src/templates/blog-post.js'),
+        context: {
+          slug: node.fields.slug,
+          nextPost: previous,
+          previousPost: next,
+        }
+      })
+    })
+  })
+}
