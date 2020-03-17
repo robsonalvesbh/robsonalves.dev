0c211dd814442954fa2030845b06ceeaf9b65940
Robson alves <robson.alves@olxbr.com>
2020-03-16 23:29:02 -0300

    config analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,145 @@ require('dotenv').config()
 
 const queries = require("./src/scripts/algoliaQuery")
 
+const pluginConfig = [
+  `gatsby-plugin-transition-link`,
+  `gatsby-plugin-styled-components`,
+  `gatsby-plugin-react-helmet`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `uploads`,
+      path: `${__dirname}/static/assets/img`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `posts`,
+      path: `${__dirname}/posts`,
+    },
+  },
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [{
+          resolve: "gatsby-remark-relative-images-v2",
+          options: {
+            name: "uploads",
+          },
+        },
+        {
+          resolve: "gatsby-remark-images",
+          options: {
+            maxWidth: 960,
+            linkImagesToOriginal: false,
+          },
+        },
+        `gatsby-remark-lazy-load`,
+        `gatsby-remark-prismjs`
+      ],
+    },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  `gatsby-plugin-sitemap`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Robson Alves`,
+      short_name: `Robson Alves`,
+      start_url: `/`,
+      background_color: `#141f2d`,
+      theme_color: `#141f2d`,
+      display: `minimal-ui`,
+      icon: `static/assets/img/robsonalves-icon.png`, // This path is relative to the root of the site.
+    },
+  },
+  {
+    resolve: `gatsby-plugin-feed`,
+    options: {
+      query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+      feeds: [
+        {
+          serialize: ({ query: { site, allMarkdownRemark } }) => {
+            return allMarkdownRemark.edges.map(edge => {
+              return Object.assign({}, edge.node.frontmatter, {
+                description: edge.node.frontmatter.description,
+                date: edge.node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                custom_elements: [{ 'content:encoded': edge.node.html }]
+              })
+            })
+          },
+          query: `
+            {
+              allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}) {
+                edges {
+                  node {
+                    fields {
+                      slug
+                    }
+                    frontmatter {
+                      title
+                      description
+                      date
+                    }
+                    excerpt(truncate: true, pruneLength: 500, format: HTML)
+                  }
+                }
+              }
+            }
+          `,
+          output: '/feed.xml',
+          title: 'Robson Alves Blog - RSS Feed'
+        }
+      ]
+    }
+  },
+  // this (optional) plugin enables Progressive Web App + Offline functionality
+  // To learn more, visit: https://gatsby.dev/offline
+  `gatsby-plugin-offline`,
+  `gatsby-plugin-netlify-cms`,
+]
+
+if (process.env.CONTEXT === 'production') {
+  const algolia = {
+    resolve: `gatsby-plugin-algolia-search`,
+    options: {
+      appId: process.env.GATSBY_ALGOLIA_APP_ID,
+      apiKey: process.env.ALGOLIA_ADMIN_KEY,
+      indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+      queries,
+      chunkSize: 10000, // default: 1000
+      enablePartialUpdates: true,
+    },
+  }
+
+  pluginConfig.push(algolia)
+
+  const analytics = {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      head: false,
+    },
+  }
+
+  pluginConfig.push(analytics)
+}
+
 module.exports = {
   siteMetadata: {
     title: `Robson Alves`,
@@ -10,127 +149,5 @@ module.exports = {
     author: `@robsonalvesdev`,
     siteUrl: 'https://robsonalves.dev'
   },
-  plugins: [
-    `gatsby-plugin-transition-link`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `uploads`,
-        path: `${__dirname}/static/assets/img`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/posts`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [{
-            resolve: "gatsby-remark-relative-images-v2",
-            options: {
-              name: "uploads",
-            },
-          },
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              maxWidth: 960,
-              linkImagesToOriginal: false,
-            },
-          },
-          `gatsby-remark-lazy-load`,
-          `gatsby-remark-prismjs`
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-algolia-search`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-        queries,
-        chunkSize: 10000, // default: 1000
-        enablePartialUpdates: true,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Robson Alves`,
-        short_name: `Robson Alves`,
-        start_url: `/`,
-        background_color: `#141f2d`,
-        theme_color: `#141f2d`,
-        display: `minimal-ui`,
-        icon: `static/assets/img/robsonalves-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.frontmatter.description,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }]
-                })
-              })
-            },
-            query: `
-              {
-                allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}) {
-                  edges {
-                    node {
-                      fields {
-                        slug
-                      }
-                      frontmatter {
-                        title
-                        description
-                        date
-                      }
-                      excerpt(truncate: true, pruneLength: 500, format: HTML)
-                    }
-                  }
-                }
-              }
-            `,
-            output: '/feed.xml',
-            title: 'Robson Alves Blog - RSS Feed'
-          }
-        ]
-      }
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify-cms`,
-  ],
+  plugins: pluginConfig
 }
