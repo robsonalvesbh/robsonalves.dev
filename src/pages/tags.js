296f575193c8e964ce921e552e908e5f99ffce5a
Robson alves <robson.alves@olxbr.com>
2020-03-16 23:29:26 -0300

    Fix components name

---
+++ src/pages/tags.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import SEO from '../components/Seo'
+import Layout from '../components/Layout'
+import PageTitle from '../components/PageTitle'
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="Tags" />
+    <PageTitle>COMING SOON</PageTitle>
+  </Layout>
+)
+
+export default AboutPage
